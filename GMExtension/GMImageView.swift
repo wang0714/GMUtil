c1abc7ee138def768533247666e86f545a3def0a
嗯嗯高明 <184724949@qq.com>
2018-07-05 17:19:57 +0800

    工具

---
+++ GMExtension/GMImageView.swift
@@ -0,0 +1,76 @@
+//
+//  GMImageView.swift
+//  GMUtil
+//
+//  Created by 汪高明 on 2018/7/5.
+//  Copyright © 2018年 gaoming. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return self.layer.borderWidth
+        }
+        set {
+            self.layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var borderColorWithUIColor: UIColor {
+        get {
+            return UIColor(cgColor:self.layer.borderColor!)
+        }
+        set {
+            self.layer.borderColor = newValue.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return self.layer.shadowOpacity
+        }
+        set {
+            self.layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor {
+        get {
+            return UIColor(cgColor:self.layer.shadowColor!)
+        }
+        set {
+            self.layer.shadowColor = newValue.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat {
+        get {
+            return self.layer.shadowRadius
+        }
+        set {
+            self.layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return self.layer.shadowOffset
+        }
+        set {
+            self.layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        }
+        set {
+            self.layer.cornerRadius = newValue
+            self.layer.masksToBounds = true
+        }
+    }
+}
---
+++ GMUtil/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GMUtil
+//
+//  Created by 汪高明 on 2018/7/5.
+//  Copyright © 2018年 gaoming. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
